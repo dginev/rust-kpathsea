cccc81140684a32f4eb6bb1248ff6f7f68c8b304
Emily Eisenberg <xymostech@gmail.com>
2019-04-11 19:27:33 -0700

    Fix bug in `guess_format_from_filename`
    It turns out that there was a bug in how I was initializing the
    formats when they weren't already initialized. I was fetching a *copy*
    of the format info struct, and then initializing the format if it
    wasn't filled in, but this didn't end up filling in the copy of the
    struct, so lookups would fail. Miraculously, in the test for this
    function, I chose to look for a `.tex` file first, which then went and
    initialized all of the formats, returned the default of
    `kpse_tex_format`, so that lookup worked, and then in the later
    lookups the formats were already initialized. Oops.
    
    This fixes the problem by fetching a reference to the format info
    struct, which then actually gets updated when `kpathsea_init_format`
    runs.
    
    Test plan: `cargo test -- --test-threads=1`

--- kpathsea/tests/find_file.rs
+++ kpathsea/tests/find_file.rs
@@ -16,8 +16,8 @@ fn it_finds_multiple_kinds_of_files() {
   let kpse = Kpaths::new()
     .expect("You need a properly setup tex toolchain (texlive/MikTeX/...) and kpathsea headers, to use this wrapper.");
 
-  assert!(kpse.find_file("plain.tex").unwrap().ends_with("plain.tex"));
   assert!(kpse.find_file("cmr10.tfm").unwrap().ends_with("cmr10.tfm"));
+  assert!(kpse.find_file("plain.tex").unwrap().ends_with("plain.tex"));
   assert!(kpse.find_file("latex.ltx").unwrap().ends_with("latex.ltx"));
   assert!(kpse.find_file("plain.mf").unwrap().ends_with("plain.mf"));
 }
